edc6a4b80db0f8163c59ff1e57b275da5fa841c5
Matthew Early <matthewjearly@gmail.com>
2017-08-27 11:01:49 -0400

    remove startup screen

--- code/scene_00_startup.cpp
+++ code/scene_00_startup.cpp
@@ -3,18 +3,18 @@
 void SceneManager::scene_00_startup() {
 	//display some kind of logo...
 
-	currentalpha = 0;
-	logo1.setAlpha(currentalpha);
-	frame_count = 0;  //reset frame count
+//	currentalpha = 0;
+//	logo1.setAlpha(currentalpha);
+//	frame_count = 0;  //reset frame count
 
-	while (frame_count < 127) {
-		Graphics_Engine.clear();
-		currentalpha += 2;
-		logo1.setAlpha(currentalpha);   //fade in logo
-		logo1.draw( ((ScreenWidth / 2) - (logo1.getWidth() / 2)), ((ScreenHeight / 2) - (logo1.getHeight() / 2)) );
-		Graphics_Engine.render();
-		frame_count++;
-	}
+//	while (frame_count < 127) {
+//		Graphics_Engine.clear();
+//		currentalpha += 2;
+//		logo1.setAlpha(currentalpha);   //fade in logo
+//		logo1.draw( ((ScreenWidth / 2) - (logo1.getWidth() / 2)), ((ScreenHeight / 2) - (logo1.getHeight() / 2)) );
+//		Graphics_Engine.render();
+//		frame_count++;
+//	}
 
 	Sound_Engine.playstartupsound();
 	scene = MAIN_MENU;
