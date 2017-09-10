1ae1b0085ca3e36a4205794ec33ca68176eb2708
Matthew Early <matthewjearly@gmail.com>
2017-09-09 21:31:22 -0400

    Removed comment

--- code/graphics.cpp
+++ code/graphics.cpp
@@ -15,7 +15,7 @@ bool SDL_graphics::init() {
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 			return false;
 		} else {
-			renderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);   //not actually vsynced in scene_99_exit.o test !??!?
+			renderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 			if (renderer == NULL) {
 				printf("SDL Failed to Create Renderer! ERROR: %s. \n", SDL_GetError());
 				return false;
